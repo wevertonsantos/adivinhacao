c779bd350265186cc7c83632ecf9bdd476c05c2b
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-11 15:03:30 -0300

    gerando while de tentativas e mudando valor de range

--- jogo_adivinhacao.py
+++ jogo_adivinhacao.py
@@ -7,15 +7,23 @@ Objetivo: O computador escolhe um número secreto entre 1 e 100. O jogador tem 5
 def main():
     numero_secreto = gerar_numero_secreto()
     numero_usuario = recebe_numero_usuario()
-    verificar_adivinhacao(numero_secreto,numero_usuario)
+    tentativas = 0
+    while tentativas < 4:
+        if verificar_adivinhacao(numero_secreto,numero_usuario):
+            print("Você acertou o número secreto")
+            break
+        else:
+            numero_usuario = recebe_numero_usuario()
+        tentativas += 1
+            
 
 def gerar_numero_secreto():
-    numero_secreto = random.randint(1,100)
+    numero_secreto = random.randint(1,50)
     return numero_secreto
 
 def recebe_numero_usuario():
     try:
-        numero_usuario = int(input("Adivinhe o número: "))
+        numero_usuario = int(input("Adivinhe o número 0 a 50: "))
         return numero_usuario
     except ValueError:
         print("Você digitou algo errado.")
@@ -28,7 +36,6 @@ def verificar_adivinhacao(numero_secreto,numero_usuario):
         print("O número foi a baixo do número secreto")
         return False
     elif numero_usuario == numero_secreto:
-        print("Você acertou o número secreto")
         return True
 
 def tentativas():
