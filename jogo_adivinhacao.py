d5fd2507b939e43fbea8e1e72f74a597c80e4e9b
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-11 15:07:33 -0300

    excluindo comentário.

--- jogo_adivinhacao.py
+++ jogo_adivinhacao.py
@@ -1,9 +1,5 @@
 import random
 
-'''
-Objetivo: O computador escolhe um número secreto entre 1 e 100. O jogador tem 5 tentativas para adivinhar. Após cada tentativa, o jogo deve indicar se o palpite foi muito alto, muito baixo ou correto.
-'''
-
 def main():
     numero_secreto = gerar_numero_secreto()
     numero_usuario = recebe_numero_usuario()
