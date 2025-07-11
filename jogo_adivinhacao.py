5972ffe754a67c9699f458c4a87ab873cb5333ec
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-11 12:26:57 -0300

    recebe_numero_usuario e verificar_adivinhacao

--- jogo_adivinhacao.py
+++ jogo_adivinhacao.py
@@ -5,10 +5,33 @@ Objetivo: O computador escolhe um número secreto entre 1 e 100. O jogador tem 5
 '''
 
 def main():
-    print(numero_secreto())
+    numero_secreto = gerar_numero_secreto()
+    numero_usuario = recebe_numero_usuario()
+    verificar_adivinhacao(numero_secreto,numero_usuario)
 
-def numero_secreto():
+def gerar_numero_secreto():
     numero_secreto = random.randint(1,100)
     return numero_secreto
+
+def recebe_numero_usuario():
+    try:
+        numero_usuario = int(input("Adivinhe o número: "))
+        return numero_usuario
+    except ValueError:
+        print("Você digitou algo errado.")
+
+def verificar_adivinhacao(numero_secreto,numero_usuario):
+    if numero_usuario > numero_secreto:
+        print("O número foi mais alto que o número secreto")
+        return False
+    elif numero_usuario < numero_secreto:
+        print("O número foi a baixo do número secreto")
+        return False
+    elif numero_usuario == numero_secreto:
+        print("Você acertou o número secreto")
+        return True
+
+def tentativas():
+    ...
 
 main()
